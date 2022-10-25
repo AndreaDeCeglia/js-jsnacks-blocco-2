f798046f885bc702c5ed034647360a476b0c55a0
AndreaDeCeglia <deceglia89@gmail.com>
2022-10-25 15:07:40 +0200

    third snack done

--- main.js
+++ main.js
@@ -31,3 +31,23 @@
 // let lastnamesN = lastnames.length;
 
 // console.log( names[ Math.round(Math.random() * (namesN - 1))] + ' ' + lastnames[ Math.round(Math.random() * (lastnamesN - 1))] );
+
+//****************************************** */
+
+
+//*Snack3*
+//Crea un array di numeri interi 
+//e fai la somma di tutti gli elementi  
+//che sono in posizione (indice) dispari
+
+let number = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+
+let som = 0;
+
+for ( let i=1; i<number.length; i+=2){
+    som += number[i];
+}
+
+console.log(number);
+
+console.log(som);
