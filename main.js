64aaee5417af3f7c4fb4b574f80877f6190eba6c
AndreaDeCeglia <deceglia89@gmail.com>
2022-10-26 17:46:30 +0200

    second snack fixed

--- main.js
+++ main.js
@@ -24,13 +24,21 @@
 //è l'unione di un "nome" e un "cognome" delle liste 
 //precedentemente dette, scelti in modo casuale.
 
-// let names = [ 'Claudiana', 'Giangilberto', 'Gigio'];
-// let namesN = names.length;
+let names = [ 'Claudiana', 'Giangilberto', 'Gigio'];
+let namesN = names.length;
 
-// let lastnames = [ 'Cognome', 'Lastname', 'Topo'];
-// let lastnamesN = lastnames.length;
+let lastnames = [ 'Cognome', 'Lastname', 'Topo'];
+let lastnamesN = lastnames.length;
 
-// console.log( names[ Math.round(Math.random() * (namesN - 1))] + ' ' + lastnames[ Math.round(Math.random() * (lastnamesN - 1))] );
+//console.log( names[ Math.round(Math.random() * (namesN - 1))] + ' ' + lastnames[ Math.round(Math.random() * (lastnamesN - 1))] );
+
+for( let i=0; i<namesN; i++){
+
+    for( let x=0; x<lastnamesN; x++){
+
+        console.log(names[i] + ' ' + lastnames[x]);
+    }
+}
 
 //****************************************** */
 
@@ -60,30 +68,30 @@
 //Aggiungi elementi all’array che ha meno elementi fino a quando 
 //ne avrà tanti quanti l’altro.
 
-let first = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
-console.log(first);
+// let first = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+// console.log(first);
 
-let second = [ 0, 1, 2, 3, 4, 5, 6];
-console.log(second);
+// let second = [ 0, 1, 2, 3, 4, 5, 6];
+// console.log(second);
 
-if( first.length > second.length ){
-    let gap = first.length - second.length;
-    for( let i=0; i<gap; i++ ){
-        let add = parseInt(prompt('add a valor'));
-        second.push(add);
-    }
-    console.log(`they're equal!!`);
-} else if( first.length < second.length ){
-    let gap = second.length - first.length;
-    for( let i=0; i<gap ; i++ ){
-        let add = parseInt(prompt('add a valor'));
-        first.push(add);
-    }
-    console.log(`they're equal!!`);
-} else{
-    console.log(`they're equal!!`);
-}
+// if( first.length > second.length ){
+//     let gap = first.length - second.length;
+//     for( let i=0; i<gap; i++ ){
+//         let add = parseInt(prompt('add a valor'));
+//         second.push(add);
+//     }
+//     console.log(`they're equal!!`);
+// } else if( first.length < second.length ){
+//     let gap = second.length - first.length;
+//     for( let i=0; i<gap ; i++ ){
+//         let add = parseInt(prompt('add a valor'));
+//         first.push(add);
+//     }
+//     console.log(`they're equal!!`);
+// } else{
+//     console.log(`they're equal!!`);
+// }
 
-console.log(first);
+// console.log(first);
 
-console.log(second);
+// console.log(second);
